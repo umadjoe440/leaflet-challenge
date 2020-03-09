7f07e0e1ce10a598caafad26b82d77798dc4d8d0
Joe Patton <umadjoe@gmail.com>
2020-03-09 18:48:17 -0400

    Removed my API Key.

--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,2 +1,2 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiamdwYXR0b240NDAiLCJhIjoiY2s3N3Vlajd0MGJ4MjNrbzVxNTd4aHk3ayJ9.7wuebq_K1wagizawfBja8Q";
+// API key Graders, please add your personal API KEY HERE
+const API_KEY = "";
